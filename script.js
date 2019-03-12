bc9d137a4cc3fff56b06dd40a93550667a259f94
vladok089 <vladok.sha089@gmail.com>
2019-03-12 12:35:51 +0200

    removed some unused code

--- script.js
+++ script.js
@@ -1,44 +1,36 @@
 class Animals {
-    constructor(name, ...expressions) {
-        this.name = name;
+    constructor(...expressions) {
         this.expressions = expressions;
     }
-    
     getNotification(i){
         var bar = document.createElement('div');
         bar.id = 'snackBar' + (i+1);
-        bar.innerHTML = (this.expressions[i]);
+        bar.innerHTML = this.expressions[i];
         document.body.appendChild(bar);
         bar.onclick = event => event.target.remove();
         setTimeout(()=>{
-                bar.remove(bar);
+          bar.remove();
         }, 3000);
     };
 }
-
 var params = {
     "fox": [
-        'Лиса',
         'Лиса хитрый зверь',
         'Он ничем не отличался от ста тысяч других лисиц',
         'Рыжий лис'
     ],
     "wolf": [
-        'Волк',
         'Волк вожак стаи',
         'Волк собирает грибы в лесу',
         'Волк меняет шкуру, но не нрав'
     ],
     "boar": [
-        'Kaбан',
         'Кабан дикий свин',
         'Кабан свинья, которая не поддалась на уговоры',
         'Кабан санитар леса'
     ]
 }
-
 var buttons = [...document.getElementsByClassName('button_showMessage')];
-
 buttons.forEach((elem, i) => {
     elem.onclick = () => {
         var select = document.getElementById("mySelect").value;
